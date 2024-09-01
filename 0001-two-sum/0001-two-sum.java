fc711613c467b0e3fed3f914302f68d41dac6ca2
RAGAV-24 <169081902+RAGAV-24@users.noreply.github.com>
2024-09-01 19:09:10 +0530

    Time: 50 ms (9.22%), Space: 44.7 MB (79.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,7 +1,5 @@
-class Solution
-{
-    public int[] twoSum(int[] nums, int target)
-    {
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
         int[] arr=new int[2];
         for(int i=0;i<nums.length;i++)
         {
