8759ac51e48498c91f8471634e047e0b740a9941
RAGAV R <169081902+RAGAV-24@users.noreply.github.com>
2024-09-12 16:10:07 +0530

    Time: 24 ms (98.03%), Space: 40.4 MB (27.58%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,26 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int l=1;
+        int r=n;
+        int m=0;
+
+       while(l<r)
+        {
+            int z=l+(r-l)/2;
+            if(isBadVersion(z))
+            {
+                r=z;
+                
+            }
+            else
+            {
+                l=z+1;
+            }
+
+        }
+        return l;
+    }
+}
