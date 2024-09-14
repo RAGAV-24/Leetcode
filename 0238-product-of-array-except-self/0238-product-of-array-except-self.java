51a635e7dec66157f7eeb139b73a6fca0882f4d5
RAGAV R <169081902+RAGAV-24@users.noreply.github.com>
2024-09-14 14:17:27 +0530

    Time: 3 ms (23.75%), Space: 55.5 MB (42.2%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,22 +1,24 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int pre[] = new int[n];
-        int suff[] = new int[n];
-        pre[0] = 1;
-        suff[n - 1] = 1;
-        
-        for(int i = 1; i < n; i++) {
-            pre[i] = pre[i - 1] * nums[i - 1];
+        int m=1;
+        int[] l=new int[nums.length];
+        int[] r=new int[nums.length];
+        l[0]=1;
+        r[nums.length-1]=1;
+        for(int i=0;i<nums.length-1;i++)
+        {
+            l[i+1]=l[i]*nums[i];
+           
         }
-        for(int i = n - 2; i >= 0; i--) {
-            suff[i] = suff[i + 1] * nums[i + 1];
+        for(int i=nums.length-1;i>0;i--)
+        {
+            r[i-1]=r[i]*nums[i];
+           
         }
-        
-        int ans[] = new int[n];
-        for(int i = 0; i < n; i++) {
-            ans[i] = pre[i] * suff[i];
-        }
-        return ans;
+       for(int i=0;i<nums.length;i++)
+       {
+        nums[i]=l[i]*r[i];
+       }
+        return nums;
     }
 }
