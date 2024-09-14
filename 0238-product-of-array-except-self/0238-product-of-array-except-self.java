a74312d65d70da319aa265cb04f08b8b1f909a3f
RAGAV R <169081902+RAGAV-24@users.noreply.github.com>
2024-09-14 14:22:26 +0530

    Time: 4 ms (7.63%), Space: 55.3 MB (56.93%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -5,16 +5,11 @@ class Solution {
         int[] r=new int[nums.length];
         l[0]=1;
         r[nums.length-1]=1;
-        for(int i=0;i<nums.length-1;i++)
-        {
-            l[i+1]=l[i]*nums[i];
-           
-        }
-        for(int i=nums.length-1;i>0;i--)
-        {
-            r[i-1]=r[i]*nums[i];
-           
-        }
+        for (int i = 0, j = nums.length - 1; i < nums.length - 1 && j > 0; i++, j--) {
+    l[i + 1] = l[i] * nums[i];
+    r[j - 1] = r[j] * nums[j];
+}
+
        for(int i=0;i<nums.length;i++)
        {
         nums[i]=l[i]*r[i];
