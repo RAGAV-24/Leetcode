1ffa8151aed546a1659d0e95af65f6164a27a747
RAGAV R <169081902+RAGAV-24@users.noreply.github.com>
2024-09-20 10:57:13 +0530

    Time: 0 ms (100%), Space: 48.9 MB (53.13%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void reverseString(char[] s) {
+       if(s.length%2==1)
+       { for(int i=0;i<(s.length-1)/2;i++)
+        {
+            char f=s[i];
+            s[i]=s[s.length-1-i];
+            s[s.length-1-i]=f;
+
+        }
+       }
+       else
+       {for(int i=0;i<(s.length)/2;i++)
+        {
+            char f=s[i];
+            s[i]=s[s.length-1-i];
+            s[s.length-1-i]=f;
+
+        }
+
+       }
+    }
+}
