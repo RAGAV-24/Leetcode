c60f221c20dddd4263086356e8e06cecdc245406
RAGAV R <169081902+RAGAV-24@users.noreply.github.com>
2024-09-23 11:15:53 +0530

    Time: 0 ms (100%), Space: 41.2 MB (22.97%) - LeetHub

---
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String toHex(int num) 
+    {
+
+    if(num==0)
+    {
+        return "0";
+
+    }
+    long val=num;
+    if(val<0)
+    {
+        val=(long)(Math.pow(2,32)+num);
+    }
+   StringBuilder sb=new StringBuilder();
+   while(val!=0)
+   {
+    
+    int rem=(int)(val%16);
+     val=val/16;
+     if(rem>=10)
+     {
+        sb.append((char)(rem+87)); 
+     }
+     else
+     {
+        sb.append(rem);
+     }
+
+    
+   }
+   return sb.reverse().toString();
+    }
+}
