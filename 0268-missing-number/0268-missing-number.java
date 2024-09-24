1798b15779c432b37f8301dac64ab37445b06fc6
RAGAV R <169081902+RAGAV-24@users.noreply.github.com>
2024-09-24 13:35:38 +0530

    Time: 5 ms (25.28%), Space: 45.1 MB (58.51%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,6 +1,5 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        HashSet<Integer> arr=new HashSet<>();
         Arrays.sort(nums);
         int k=0;
         for(int i=0;i<nums.length;i++)
