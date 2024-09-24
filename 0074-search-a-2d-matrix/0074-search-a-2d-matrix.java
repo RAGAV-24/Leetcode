a9ebd6818deb733f2831f1b5324daa1ba3985ceb
RAGAV R <169081902+RAGAV-24@users.noreply.github.com>
2024-09-24 11:11:21 +0530

    Time: 0 ms (100%), Space: 42.4 MB (18.9%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        int k=0;
         for(int i=0;i<matrix.length;i++)
         {
             for(int j=0;j<matrix[0].length;j++)
@@ -10,8 +11,14 @@ class Solution {
                 }
                 if(matrix[i][j]>target)
                 {
+                     k=k+1;
                     break;
+                   
                 }
+            }
+            if(k==1)
+            {
+                break;
             }
         }
         return false;
