832d8b340c7c807a1a07e6fca05bf651a66f2c44
RAGAV R <169081902+RAGAV-24@users.noreply.github.com>
2024-09-24 14:53:34 +0530

    Time: 1 ms (88.54%), Space: 41.4 MB (70.67%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
@@ -3,21 +3,8 @@ class Solution {
         int c=0;
         for(int i=0;i<=s.length()-3;i++)
         {
-           StringBuilder arr=new StringBuilder(s.substring(i,i+3));
-           char[] ar=arr.toString().toCharArray();
-        int f=0;
-        for(int j=0;j<1;j++)
-        {
-            if(ar[0] != ar[1] && ar[1] != ar[2] && ar[0] != ar[2])
-            {
-                f=f+1;
-            }
-
-        }
-        if(f==1)
-        {
-            c=c+1;
-        }
+           if(s.charAt(i)!=s.charAt(i+1)&&s.charAt(i+1)!=s.charAt(i+2) && s.charAt(i)!=s.charAt(i+2))
+           c=c+1;
         }
         return c;
     }
