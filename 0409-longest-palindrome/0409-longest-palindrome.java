92e9b86c1b80580b12089bd2753b6ef3ce5c34f4
RAGAV R <169081902+RAGAV-24@users.noreply.github.com>
2024-09-26 14:42:25 +0530

    Time: 20 ms (6.79%), Space: 42.4 MB (6.72%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char f:s.toCharArray())
+        {
+            map.put(f,map.getOrDefault(f,0)+1);
+        }
+        int c=0;
+       for(char a:map.keySet())
+       {
+        if(map.get(a)%2==0)
+        {
+            c=c+map.get(a);
+        }
+       }
+       for(char a:map.keySet())
+       {
+        System.out.println(a+" "+map.get(a));
+       }
+       int r=0;
+       for(char a:map.keySet())
+       {
+        
+         if(map.get(a)%2==1 && map.get(a)>2 &&r==0)
+        {
+            
+            c=c+map.get(a);
+            r=1;
+        }
+        else if(map.get(a)%2==1 && map.get(a)<2 &&r==0)
+        {
+            c=c+map.get(a);
+            r=1;
+        }
+        else if(map.get(a)%2==1 && map.get(a)>2 &&r==1)
+        {
+            
+            c=c+map.get(a)-1;
+            
+        }
+       }
+    
+       return c;
+    }
+}
