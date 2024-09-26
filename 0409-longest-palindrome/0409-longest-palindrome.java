d622078290904ad1be1aaeb7dbe0fc6f3e247b26
RAGAV R <169081902+RAGAV-24@users.noreply.github.com>
2024-09-26 14:46:46 +0530

    Time: 8 ms (28.81%), Space: 41.7 MB (54.41%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -13,10 +13,7 @@ class Solution {
             c=c+map.get(a);
         }
        }
-       for(char a:map.keySet())
-       {
-        System.out.println(a+" "+map.get(a));
-       }
+       
        int r=0;
        for(char a:map.keySet())
        {
@@ -27,7 +24,7 @@ class Solution {
             c=c+map.get(a);
             r=1;
         }
-        else if(map.get(a)%2==1 && map.get(a)>2 &&r==1)
+        else if(map.get(a)%2==1  &&r==1)
         {
             
             c=c+map.get(a)-1;
