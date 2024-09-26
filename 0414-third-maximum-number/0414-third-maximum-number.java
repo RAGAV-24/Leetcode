6916b891695b4b54e692cfdd52ff46c5577feae5
RAGAV R <169081902+RAGAV-24@users.noreply.github.com>
2024-09-26 11:22:13 +0530

    Time: 5 ms (35.63%), Space: 45.1 MB (6.79%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,27 +1,15 @@
 class Solution {
     public int thirdMax(int[] nums) {
-       long a=Long.MIN_VALUE;
-       long b=Long.MIN_VALUE;
-       long c=Long.MIN_VALUE;
-       for(int i=0;i<nums.length;i++)
+       TreeSet<Integer> ee=new TreeSet<>();
+       for(int i:nums)
        {
-        if(nums[i]>a)
-        {
-           c=b;
-           b=a;
-           a=nums[i];
-        }
-        else if(nums[i]>b&&a>nums[i])
-        {
-
-            c=b;
-            b=nums[i];
-        }
-        else if(nums[i]>c && b>nums[i])
-        {
-            c=nums[i];
-        }
+        ee.add(i);
        }
-       return c!=Long.MIN_VALUE?(int)c:(int)a;
+       if(ee.size()>2)
+       {
+        ee.pollLast();
+        ee.pollLast();
+       }
+       return ee.last();
     }
 }
