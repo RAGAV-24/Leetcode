750ceb789859531002df8449866cf0427d4c9822
RAGAV R <169081902+RAGAV-24@users.noreply.github.com>
2024-10-07 17:19:36 +0530

    Time: 0 ms (100%), Space: 45.5 MB (50.67%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,10 +1,12 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int[] row=new int[matrix.length];
-        int[] col=new int[matrix[0].length];
-        for(int i=0;i<row.length;i++)
+         int n = matrix.length;
+        int m = matrix[0].length;
+        int[] row=new int[n];
+        int[] col=new int[m];
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<col.length;j++)
+            for(int j=0;j<m;j++)
             {
                 if(matrix[i][j]==0)
                 {
@@ -13,9 +15,9 @@ class Solution {
                 }
             }
         }
-        for(int i=0;i<row.length;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<col.length;j++)
+            for(int j=0;j<m;j++)
             {
                 if(row[i]==1 ||col[j]==1)
                 {
