98510fd980325c40e570d10cbc93d33516b3e0eb
RAGAV R <169081902+RAGAV-24@users.noreply.github.com>
2025-03-12 22:20:39 +0530

    Time: 62 ms (69.54%), Space: 57.4 MB (85.12%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] dailyTemperatures(int[] t) {
+        int n = t.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && t[i] > t[stack.peek()]) {
+                int idx = stack.pop();
+                res[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
